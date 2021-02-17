c04388395eefdf3a813ab51498ac6f4df832e410
Milena Grncarova <milenagrncarov@outlook.com>
2021-02-17 08:27:10 +0100

    Made the card popup

--- app.js
+++ app.js
@@ -7,18 +7,55 @@ function fetchData(){
       return response.json();
   })
   .then(data => {
-    const html = data
-    .map(userTemplate).join("");
+    pictures = data;
+    const html = iterator(data,0);
     document.querySelector("#app").insertAdjacentHTML("afterbegin",html);
+    var elements = document.getElementsByClassName("container");
+    for(var i=index; i<elements.length; i++) {
+      index = i;
+      elements[i].addEventListener("click",(card)=> {
+        document.querySelector("#pop").insertAdjacentHTML("afterbegin", showPopup(data[card.target.id]));
+        });
+   }
+
   })
   .catch(error => {
     console.log(error)
   })
 }
 
-function userTemplate(user){
+
+function showPopup(user){
   return`
-    <a href="" class="cards" id="card">
+  <div onClick="destroy()" class="popup">
+  <div class="popup-content">
+    <img class="popup-image"src="${user.image}" alt="image"/>
+    <div class="popup-desc">
+      <div class= "top-part">
+        <img class="avatar"src="${user.profile_image}" alt="profile-image"/>
+        <div class ="top-part-content">
+          <p><strong>${user.name}</strong><p>
+          <p>${user.date}</p>
+        </div>
+        <img src="icons/instagram-logo.svg" alt="instagram-logo"/>
+      </div>
+      <hr>
+      <div class="middle-part">
+        <p>#${user.caption}</p>
+      </div>
+      <div class="bottom-part">
+        <img src="icons/heart.svg" alt="heart-logo"/>
+        ${user.likes}
+      </div>
+    </div>
+  </div>
+</div>
+  `
+}
+
+function userTemplate(user,i){
+  return`
+    <a href="" id="${i}"class="container">
       <div class= "top-part">
         <img class="avatar"src="${user.profile_image}" alt="profile-image"/>
         <div class ="top-part-content">
@@ -38,29 +75,6 @@ function userTemplate(user){
       </div>
     </a>
     
-    <div class="popup">
-      <div class="popup-content">
-        <img class="popup-image"src="${user.image}" alt="image"/>
-        <div class="popup-desc">
-          <div class= "top-part">
-            <img class="avatar"src="${user.profile_image}" alt="profile-image"/>
-            <div class ="top-part-content">
-              <p><strong>${user.name}</strong><p>
-              <p>${user.date}</p>
-            </div>
-            <img src="icons/instagram-logo.svg" alt="instagram-logo"/>
-          </div>
-          <hr>
-          <div class="middle-part">
-            <p>#${user.caption}</p>
-          </div>
-          <div class="bottom-part">
-            <img src="icons/heart.svg" alt="heart-logo"/>
-            ${user.likes}
-          </div>
-        </div>
-      </div>
-    </div>
     `
 }
 
